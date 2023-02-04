ce6cbbeeea3761450e4d31481dc558c73d76dd93
Eduardo_Morales <79502445+eduardomorales2019@users.noreply.github.com>
2023-02-04 14:17:00 -0600

    se agregan mas xambios

--- includes/front/setup.php
+++ includes/front/setup.php
@@ -6,6 +6,8 @@ add_theme_support('editor-styles');
 add_editor_style([
 		"https://fonts.googleapis.com/css2?family=Pacifico&family=Rubik:wght@300;400;500;700&display=swap",
 		"assets/theme 2/bootstrap-icons/bootstrap-icons.css",
-		"/assets/theme 2/public/index.css"
+		"/assets/theme 2/public/index.css",
+		"assets/editor.css
+		"
 	]); 
 }
